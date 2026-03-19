3e325f15783d12a4e93deb7b0de7084279e10cfe
Prof.T Tech <95409350+HamzatAbdulazeez@users.noreply.github.com>
2026-03-19 03:02:26 +0100

    “updating”

--- app/Layout/Page/Banner.tsx
+++ app/Layout/Page/Banner.tsx
@@ -27,12 +27,12 @@ const Banner = () => {
         {/* Hero Section */}
         <div className="flex flex-1 items-center justify-center text-center px-6">
           <div className="max-w-4xl">
-            <h1 className="text-4xl md:text-5xl lg:text-7xl font-extrabold leading-tight">
+            <h1 className="text-4xl md:text-5xl lg:text-7xl font-extrabold animate-fadeUp leading-tight">
               We Ensure better education <br className="hidden md:block" />
               for a better world
             </h1>
 
-            <p className="mt-6 text-sm md:text-lg lg:text-xl font-light">
+            <p className="mt-6 text-sm md:text-lg lg:text-xl animate-fadeUp font-light">
               Our cutting-edge curriculum is designed to empower students with
               the knowledge, skills, and experiences needed to excel in the
               dynamic field of education
@@ -40,7 +40,7 @@ const Banner = () => {
 
             {/* Button */}
             <div className="flex justify-center mt-8">
-              <button className="flex items-center gap-3 bg-white text-black rounded-full px-6 py-3 font-medium hover:bg-gray-200 transition">
+              <button className="flex animate-fadeUp items-center gap-3 bg-white text-black rounded-full px-6 py-3 font-medium hover:bg-gray-200 transition">
                 Explore More <MoveRight className="" size={20} />
               </button>
             </div>
--- app/Layout/Page/Program.tsx
+++ app/Layout/Page/Program.tsx
@@ -10,10 +10,10 @@ const Program = () => {
         <b className="text-2xl md:text-3xl font-bold m-5">What We Offer </b>
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-6 max-w-6xl mx-auto pb-5 ">
-          <div className="relative group overflow-hidden rounded-2xl">
+          <div className="relative group overflow-hidden  rounded-2xl">
             <img
               src="https://res.cloudinary.com/dbmtogyef/image/upload/v1773925629/program-1-DA_EvTPy_t1cwte.png"
-              className="w-full rounded-2xl hover:bg-blue-900/40"
+              className="w-full animate-fadeIn delay-100  rounded-2xl hover:bg-blue-900/40"
               alt=""
             />
             {/* Blue Overlay */}
@@ -23,7 +23,7 @@ const Program = () => {
           <div  className="relative group overflow-hidden rounded-2xl">
             <img
               src="https://res.cloudinary.com/dbmtogyef/image/upload/v1773925681/program-3-C0g4YULi_dfuy98.png"
-              className="w-full rounded-2xl hover:bg-blue-900/40"
+              className="w-full animate-fadeIn delay-100  rounded-2xl hover:bg-blue-900/40"
               alt=""
             />
               {/* Blue Overlay */}
@@ -33,7 +33,7 @@ const Program = () => {
           <div  className="relative group overflow-hidden rounded-2xl">
             <img
               src="https://res.cloudinary.com/dbmtogyef/image/upload/v1773925702/program-2-DRqwF0EM_khniut.png"
-              className="w-full rounded-2xl hover:bg-blue-900/40"
+              className="w-full animate-fadeIn delay-100 rounded-2xl hover:bg-blue-900/40"
               alt=""
             />
               {/* Blue Overlay */}
